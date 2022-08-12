a1b584c0598b1cfa6f8bafdf77cf4baa24043fa7
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-12 22:20:11 +0600

    quiz preactis explore

--- arguments.js
+++ arguments.js
@@ -3,3 +3,36 @@ function add(num1, num2) {
     console.log(arguments); // Arguments holi array kike object aita kintu object na....
 }
 add(12, 13, 44, 76, 73, 64, 98, 99);
+
+
+// quiz explore;
+
+function compare(a, b) {
+    if (a == b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+const result = compare(15, "15");
+console.log(result);
+
+/* 
+function compare(a, b) {
+    if (a.toString() == b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+const result = compare(15, 15);
+console.log(result);
+
+*/
+console.log('123' + 123);
+
+let a = 'hi'
+let b = 'there'
+console.log(a + b);
